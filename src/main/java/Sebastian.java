e8ca656d0534aa4bc7211ccb6f3e351799022c9c
Winslowthedogg <sebas4idua@gmail.com>
2022-09-01 17:43:41 -0400

    added code

---
+++ src/main/java/Sebastian.java
@@ -0,0 +1,30 @@
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+
+@Getter
+
+
+public class Sebastian {
+
+    private Integer age;
+
+ private LinkedHashMap<BigDecimal, Boolean> toDoList;
+
+    protected static String title;
+
+
+
+
+    public Sebastian(Integer age){
+        setAge(age);
+    }
+
+    private void setAge(Integer age) {
+       if (age < -18) throw new RuntimeException();
+       this.age = age;
+    }
+
+
+}
