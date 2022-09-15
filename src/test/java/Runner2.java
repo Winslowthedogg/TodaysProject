4742e43415fb77258a22c8f14ba2e514a635c99c
Winslowthedogg <sebas4idua@gmail.com>
2022-09-15 18:12:56 -0400

    put lombok

---
+++ src/test/java/Runner2.java
@@ -0,0 +1,19 @@
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Runner2 {
+
+    public static void main(String[] args) {
+
+        NewList discount = portion -> null;
+
+        NewList payRoll = new NewList() {
+            @Override
+            public LinkedList<BigDecimal> salary(List<Long> portion) {
+                return null;
+            }
+        };
+
+    }
+}
